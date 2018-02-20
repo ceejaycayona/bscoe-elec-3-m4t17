b6679b4e2098e02861acd0757eee53e29874089e
ceejaycayona <34419457+ceejaycayona@users.noreply.github.com>
2018-02-20 10:56:02 +0800

    Add files via upload

---
+++ Rock1 Texture/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+	bool input;
+	// Use this for initialization
+	void Start () {
+		input = true;	
+	}
+	
+	void LoadGameScene(){
+		
+		SceneManager.LoadScene("GameScene");
+	}
+	void Awake(){
+		
+		DontDestroyOnLoad (gameObject);
+	
+	}
+	void Update () {
+		
+		if (Input.anyKey && input) {
+			Invoke ("LoadGameScene", 1f);
+			input = false;
+		}
+	}
+}
