74ada4cd1be4be7d8b2da6049f7b55e02a9623fc
ceejaycayona <34419457+ceejaycayona@users.noreply.github.com>
2018-02-27 19:38:16 +0800

    add enemycollision and explosion wav

---
+++ enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+	public ParticleSystem ExplodeEnemy;
+
+	void onParticleCollision(GameObject other){
+		if (other.gameObject.tag == "bullet") {
+			ExplodeEnemy.Play ();
+			Destroy(other, 1f);
+			print ("boom");
+		
+		}
+
+	}
+}
